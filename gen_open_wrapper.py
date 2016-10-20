ac5dd54c64645478815dc35e7efd679ca3883939
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-10-20 17:45:06 -0500

    rename and tidy up ast example

--- gen_open_wrapper.py
+++ gen_open_wrapper.py
@@ -1,26 +1,21 @@
 import sys
 from pycparser import c_parser, c_ast, c_generator
 
-text = r"""
-int open(const char *pathname, int flags);
-"""
+# example method signature
+text = "int open(const char *pathname, int flags);"
 
+# parse the AST
 parser = c_parser.CParser()
 ast = parser.parse(text, filename='<none>')
 
 decl_node = ast.ext[-1]
-print("name: %s" % decl_node.name)
-
 decl = decl_node.type
 gen = c_generator.CGenerator()
-print("whole sig: %s" % gen.visit(decl))
-print("return: %s" % gen.visit(decl.type))
-for param in decl.args.params:
-    print("param: %s" % gen.visit(param))
-
 
 with open('open_wrapper.c', 'w') as f:
-    f.write('#define _GNU_SOURCE\n#include <dlfcn.h>\n#include <stdio.h>\n\n')
+    f.write('#define _GNU_SOURCE\n')
+    f.write('#include <dlfcn.h>\n')
+    f.write('#include <stdio.h>\n\n')
     f.write('static %s (*real_open) (%s) = NULL;\n\n' % (gen.visit(decl.type), ', '.join([gen.visit(param) for param in decl.args.params])))
     f.write('%s {\n' % gen.visit(decl))     # int open(const char *pathname, int flags) {
     f.write('  printf("wrapped open\\n");\n')  # 
