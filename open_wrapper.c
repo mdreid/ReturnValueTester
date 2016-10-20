2285131ab2eee0e300326dc498594d351c443e46
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-10-20 12:55:11 -0500

    add randomness to open()

--- open_wrapper.c
+++ open_wrapper.c
@@ -1,11 +1,24 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <errno.h>
+#include "rng.c"
 
 static int (*real_open) (const char *pathname, int flags) = NULL;
 
 int open(const char *pathname, int flags) {
-	printf("wrapped open\n");
-	real_open = dlsym(RTLD_NEXT, "open");
-	return real_open(pathname, flags);
+  // initialize the rng
+  rand_setup();
+
+  printf("wrapped open\n");
+  real_open = dlsym(RTLD_NEXT, "open");
+  
+  // flip a coin
+  if(rand_bool()) {
+    printf("  open failed\n");
+    errno = ENFILE;
+    return -1;
+  } else {
+    return real_open(pathname, flags);
+  }
 }
