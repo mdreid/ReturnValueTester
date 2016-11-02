f09ac3ca34c162257d223f9c988880426b46641c
Colin Samplawski <colnski03@gmail.com>
2016-11-02 13:59:13 -0500

    changed open wrapper

--- wrappers/open_wrapper.c
+++ wrappers/open_wrapper.c
@@ -3,19 +3,15 @@
 #include <stdio.h>
 #include <errno.h>
 #include "rng.h"
-int c = 0;
 
 static int (*real_open) (const char *pathname, int flags) = NULL;
 
 int open(const char *pathname, int flags) {
-	int rand_num = rand_bool(.01);
+	int rand_num = rand_bool(.1);
 	if (rand_num) {
-		printf("%d returning an error\n", c);
-		c++;
 		errno = EACCES;
 		return -1;
 	} else {
-		//printf("called\n");
 		real_open = dlsym(RTLD_NEXT, "open");
 		return real_open(pathname, flags);
 	}
