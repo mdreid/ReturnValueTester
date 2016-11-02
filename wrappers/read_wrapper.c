156415e6774036b39a75002357bd6f3fb11440b6
Colin Samplawski <colnski03@gmail.com>
2016-11-02 14:29:12 -0500

    check dlsym in read wrapper

--- wrappers/read_wrapper.c
+++ wrappers/read_wrapper.c
@@ -10,12 +10,11 @@ static ssize_t (*real_read) (int fd, void *buf, size_t count) = NULL;
 ssize_t read(int fd, void *buf, size_t count) {
 	int rand = rand_bool(.1);
 	//printf("rand num is %d\n", rand);
-	if (rand) {
+	real_read = dlsym(RTLD_NEXT, "read");
+	if (rand || (real_read == NULL)) {
 		errno = EBADF;
 		return -1;
 	} else {
-		//printf("wrapped read\n");				//do something before calling real read
-		real_read = dlsym(RTLD_NEXT, "read"); 	//get addr of real read
 		return real_read(fd, buf, count); 		//call real read
 	}
 }
