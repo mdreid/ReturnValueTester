36d700dc29b268de11330701a18823f149691124
colin <colnski03@gmail.com>
2016-12-03 18:21:44 -0600

    net-tools stuff

--- test_dir/test.sh
+++ test_dir/test.sh
@@ -8,22 +8,18 @@ do
 done
 
 while read -r line; do
-#line="base64|rm -f tmp.txt|echo 'testing base64' >> tmp.txt|tmp.txt"
-	#echo $line
 	export PRE=
 	export AFTER=
 	for prob in {0..50..10}; do
 		percent=$(echo print $prob / 100. | python)
+		export PROB=$percent
 		echo [$line $percent]
 		IFS='|' read -ra OPTS <<< "$line"
 		IFS=' ' read -ra ARGS <<< "${OPTS[3]}"
 		for call in "${CALLS[@]}"; do
 			export PRE=${OPTS[1]}
 			export AFTER=${OPTS[2]}
-			#echo $PRE
-			.././log_gen 20 $call ${OPTS[0]} ${ARGS[@]} $percent
-			#echo $AFTER
-			#echo $PRE
+			.././log_gen 20 $call ${OPTS[0]} ${ARGS[@]}
 		done
 	done
 done < $1
