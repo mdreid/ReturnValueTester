417318e058c7342cc62f0717ffe986a3f11f5098
Colin Samplawski <colnski03@gmail.com>
2017-02-09 16:31:24 -0600

    added an open wrapper

---
+++ wrappers/open_wrapper.c
@@ -0,0 +1,19 @@
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+static int (*real_open) (const char *pathname, int flags) = NULL;
+extern int open(const char *pathname, int flags) {
+  char* var = getenv("PROB");
+  float p = atof(var);
+  int flip = rand_bool((double) p);
+  real_open = dlsym(RTLD_NEXT, "open");
+  if(flip || (real_open == NULL)) {
+    errno = EACCES;
+    return -1;
+  } else {
+    return real_open(pathname, flags);
+  }
+}
