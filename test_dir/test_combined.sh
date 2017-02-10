aaf8f56fc038253b87d941a5c9593ad24eb08367
Colin Samplawski <colnski03@gmail.com>
2017-02-10 15:44:39 -0600

    added test_combined.sh

---
+++ test_dir/test_combined.sh
@@ -0,0 +1,30 @@
+CALLS=()
+for w in ../wrappers/*.so
+do
+	call=$(basename "$w")
+	call="${call%.*}"
+	call="${call%_*}"
+	CALLS+=($call)
+done
+
+while read -r line; do
+        # skip comment lines
+        case "$line" in \#*) continue ;; esac
+
+	export PRE=
+	export AFTER=
+	for prob in {0..50..10}; do
+		percent=$(echo print $prob / 100. | python)
+		export PROB=$percent
+		echo [$line $percent]
+		IFS='|' read -ra OPTS <<< "$line"
+		IFS=' ' read -ra ARGS <<< "${OPTS[3]}"
+		for call in "${CALLS[@]}"; do
+			export PRE=${OPTS[1]}
+			export AFTER=${OPTS[2]}
+			.././log_gen 20 $call ${OPTS[0]} ${ARGS[@]}
+		done
+	done
+done < $1
+export PRE=
+export AFTER=
